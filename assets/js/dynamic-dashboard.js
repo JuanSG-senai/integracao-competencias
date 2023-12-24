2eafe4cd12c2744f96a9f5e2f7aebd60ecc8084b
juan <juangomes.profissional@gmail.com>
2023-12-24 17:32:19 -0300

    Header com modal 100% funcional

--- assets/js/dynamic-dashboard.js
+++ assets/js/dynamic-dashboard.js
@@ -2,28 +2,37 @@
 
 export const userDashboard = () => {
     let user = JSON.parse(localStorage.getItem('user'));
+    const body = document.getElementsByTagName('body')[0];
 
+    // HEADER COM MODAL
     const header = document.createElement('header');
     header.innerHTML = `
-    <nav>
+    <nav class="nav">
         <h1>JN Eventos</h1>
-        <div id="userNav">
-            <p>${user.username}</p>
-            <div id="navModal" style="display: none;">
+        <div>
+            <p id="userNav">${user.username}</p>
+            <div id="navModal">
                 <span>Nome de usuário  <p>${user.username}</p></span>
                 <span>Nome  <p>${user.name}</p></span>
                 <span>Sobrenome  <p>${user.surname}</p></span>
                 <span>Email  <p>${user.email}</p></span>
                 <span>Idade  <p>${user.age}</p></span>
+                <button id="closeModal">Fechar</button>
             </div>
         </div>
     </nav>
     `;
-    
-    const body = document.getElementsByTagName('body')[0];
+
     body.appendChild(header);
 
+    const navModal = document.getElementById('navModal');
     document.getElementById('userNav').onclick = () => {
-        document.getElementById('navModal').style.display = 'initial';
+        navModal.style.display = 'flex';
+        body.style.pointerEvents = 'none';
+        navModal.style.pointerEvents = 'auto';
+    };
+    document.getElementById('closeModal').onclick = () => {
+        navModal.style.display = 'none';
+        body.style.pointerEvents = 'auto';
     };
 };
